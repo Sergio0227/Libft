85c7cd0a385a119e4629da802aab770d788b3613
Sergio Filipe Andre Aires <sandre-a@c1r6s4.42luxembourg.lu>
2024-09-08 18:37:56 +0200

    added prototype free_ft_split

--- libft.h
+++ libft.h
@@ -68,5 +68,6 @@ t_list				*ft_lstlast(t_list *lst);
 t_list				*ft_lstnew(void *content);
 t_list				*ft_lstmap(t_list *lst, void *(*f)(void *),
 						void (*del)(void *));
+void				free_ft_split(char **strings);
 
 #endif
