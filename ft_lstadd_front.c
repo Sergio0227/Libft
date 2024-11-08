1413c726ee317d184aa521acd226b782524ab3dd
Sergio Aires <sergioaires@gmail.com>
2024-11-08 11:54:02 +0100

    bug fix lst_add_front

--- ft_lstadd_front.c
+++ ft_lstadd_front.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   ft_lstadd_front.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sandre-a <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: sandre-a <sandre-a@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/28 23:22:25 by sandre-a          #+#    #+#             */
-/*   Updated: 2024/09/22 22:53:12 by sandre-a         ###   ########.fr       */
+/*   Updated: 2024/11/08 11:53:33 by sandre-a         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-* The ft_lstadd_front function adds a new node at the beginning of a list.
+ * The ft_lstadd_front function adds a new node at the beginning of a list.
 */
 void	ft_lstadd_front(t_list **lst, t_list *new)
 {
@@ -21,6 +21,8 @@ void	ft_lstadd_front(t_list **lst, t_list *new)
 	{
 		new->prev = NULL;
 		new->next = *lst;
+		if (*lst)
+			(*lst)->prev = new;
 		(*lst) = new;
 	}
 }
